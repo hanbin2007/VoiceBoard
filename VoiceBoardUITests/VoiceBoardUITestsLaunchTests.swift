f5fbc2181ca241a9c3b7c8babc7b025f75043f5b
ZhangHanbin <thebestzhq@gmail.com>
2026-01-04 15:59:58 +0800

    Initial Commit

---
+++ VoiceBoardUITests/VoiceBoardUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  VoiceBoardUITestsLaunchTests.swift
+//  VoiceBoardUITests
+//
+//  Created by zhb on 2026/1/4.
+//
+
+import XCTest
+
+final class VoiceBoardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
